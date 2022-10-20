a4a250ac10736b74f6247e4ebd131cd4722192c0
stav05 <stav.armoni-friedmann@kcl.ac.uk>
2022-10-20 15:49:19 +0100

    implement frequency counter

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,16 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+
+
+
+
+    for item in items:
+        if(not isinstance(item, str)):
+            item = str(item)
+        if(frequencies.get(item)):
+            frequencies[item] = frequencies[item] + 1
+        else:
+            frequencies.update({item: 1})
+
     return frequencies
